cd15e549b4b7653ec763755c99184a69675c349a
Marlon <marlonquinteri274@gmail.com>
2024-12-11 11:19:37 -0500

    Update task1.js

--- task1.js
+++ task1.js
@@ -17,6 +17,3 @@ console.log(costCalculator(monto)); // Salida: 4
 
 
 
-// * 1. María está calculando el costo de los pagos mensuales. Por cada transacción hay una tarifa de $3 y una tarifa de interés del 1% (0,01). 
-//     * Dado el monto de una transacción de entrada, exporte una función que devuelva el valor de lo que debería pagar.
-//         * Esta función debería poder tomar un número como entrada y devolver un número como salida.
